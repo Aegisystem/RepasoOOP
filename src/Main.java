858c1a33c51dee0ecd358042bcae443515214d57
Steven Correa <steven14correa@gmail.com>
2026-02-06 09:05:08 -0500

    Implementación de herencia y polimorfisco

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,13 @@ public class Main {
         CuentaBancaria cuenta1 = new CuentaBancaria("1", 100);
         cuenta1.retirar(100);
         cuenta1.consignar(150);
-        System.out.printf("El saldo de la cuenta es: " + cuenta1.getSaldo());
+        System.out.println("El saldo de la cuenta es: " + cuenta1.getSaldo());
+
+        Notificacion[] notificaciones = new Notificacion[2];
+        notificaciones[0] = new NotificacionCorreo("Pepito");
+        notificaciones[1] = new NotificacionSMS("Juanito");
+
+        notificaciones[0].enviarMensaje("Hola mi amigo");
+        notificaciones[1].enviarMensaje("Hola mi broco");
     }
 }
